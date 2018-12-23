ab62aa257ea7a2709154702f99d188d9ecf81112
Ilimkan Omurzakova <ilima.vip@gmail.com>
2018-12-23 13:02:14 -0500

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ObservablesComponent } from './observables/observables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent
+  },
+  {
+    path: 'overview/observables',
+    component: ObservablesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
